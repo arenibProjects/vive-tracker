8bc660b9cf4f46e839d38d5a0a65175710f4af0e
Evan Roué <roue.evan1@gmail.com>
2018-03-29 17:14:36 +0200

    Add Position_finder module

---
+++ code/ViveBeacons/ViveBeacons.cydsn/Position_finder.h
@@ -0,0 +1,28 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+#if !defined(POSITION_FINDER_H)
+#define POSITION_FINDER_H
+
+// Attributes
+typedef struct Position_finder Position_finder;
+struct Position_finder {
+    
+};
+
+// Public methods
+Position_finder* Position_finder_create();
+void Position_finder_init(Position_finder *position_finder);
+
+#endif
+
+/* [] END OF FILE */
