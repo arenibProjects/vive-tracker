acfdeb8c983d300def3b2dd30b5ca2e06e4689bf
Evan Roué <roue.evan1@gmail.com>
2018-03-29 17:08:45 +0200

    vive_sensors module completed

--- code/ViveBeacons/ViveBeacons.cydsn/vive_sensors.c
+++ code/ViveBeacons/ViveBeacons.cydsn/vive_sensors.c
@@ -10,169 +10,114 @@
  * ========================================
 */
 
-#include "vive_sensors.h"
+#include <stdlib.h>
+#include "configuration.h"
+#include "VIVE_sensors.h"
 
-void vive_sensors_init(vive_sensors *vive_sensors) {
-    /* Variable declarations for DMA_timing_read */
-    /* Move these variable declarations to the top of the function */
-    uint8 DMA_timing_read_Chan;
-    uint8 DMA_timing_read_TD[16];
+/*
+    --------------
+    Public methods
+    --------------
+*/
 
-    /* DMA Configuration for DMA_timing_read */
-    DMA_timing_read_Chan = DMA_timing_read_DmaInitialize(DMA_timing_read_BYTES_PER_BURST, DMA_timing_read_REQUEST_PER_BURST,
+VIVE_sensors* VIVE_sensors_create() {
+    VIVE_sensors* vive_sensors = (VIVE_sensors*) malloc(1*sizeof(VIVE_sensors));
+    
+    // TS4231 drivers creation
+    vive_sensors->ts4231_drivers[0] = TS4231_driver_create(enveloppe_pins_0, data_pins_0);
+    vive_sensors->ts4231_drivers[1] = TS4231_driver_create(enveloppe_pins_1, data_pins_1);
+    vive_sensors->ts4231_drivers[2] = TS4231_driver_create(enveloppe_pins_2, data_pins_2);
+    vive_sensors->ts4231_drivers[3] = TS4231_driver_create(enveloppe_pins_3, data_pins_3);
+    vive_sensors->ts4231_drivers[4] = TS4231_driver_create(enveloppe_pins_4, data_pins_4);
+    vive_sensors->ts4231_drivers[5] = TS4231_driver_create(enveloppe_pins_5, data_pins_5);
+    vive_sensors->ts4231_drivers[6] = TS4231_driver_create(enveloppe_pins_6, data_pins_6);
+    vive_sensors->ts4231_drivers[7] = TS4231_driver_create(enveloppe_pins_7, data_pins_7);
+    
+    return vive_sensors;
+}
+
+void VIVE_sensors_init(VIVE_sensors *vive_sensors) {
+    // DMA Configuration for DMA_timing_read
+    
+    // DMA Channel initialization
+    vive_sensors->DMA_timing_read_Chan = DMA_timing_read_DmaInitialize(DMA_timing_read_BYTES_PER_BURST, DMA_timing_read_REQUEST_PER_BURST,
         HI16(DMA_timing_read_SRC_BASE), HI16(DMA_timing_read_DST_BASE));
-    DMA_timing_read_TD[0] = CyDmaTdAllocate();
-    DMA_timing_read_TD[1] = CyDmaTdAllocate();
-    DMA_timing_read_TD[2] = CyDmaTdAllocate();
-    DMA_timing_read_TD[3] = CyDmaTdAllocate();
-    DMA_timing_read_TD[4] = CyDmaTdAllocate();
-    DMA_timing_read_TD[5] = CyDmaTdAllocate();
-    DMA_timing_read_TD[6] = CyDmaTdAllocate();
-    DMA_timing_read_TD[7] = CyDmaTdAllocate();
-    DMA_timing_read_TD[8] = CyDmaTdAllocate();
-    DMA_timing_read_TD[9] = CyDmaTdAllocate();
-    DMA_timing_read_TD[10] = CyDmaTdAllocate();
-    DMA_timing_read_TD[11] = CyDmaTdAllocate();
-    DMA_timing_read_TD[12] = CyDmaTdAllocate();
-    DMA_timing_read_TD[13] = CyDmaTdAllocate();
-    DMA_timing_read_TD[14] = CyDmaTdAllocate();
-    DMA_timing_read_TD[15] = CyDmaTdAllocate();
-    CyDmaTdSetConfiguration(DMA_timing_read_TD[0],  2, DMA_timing_read_TD[1], CY_DMA_TD_AUTO_EXEC_NEXT);
-    CyDmaTdSetConfiguration(DMA_timing_read_TD[1],  2, DMA_timing_read_TD[2], CY_DMA_TD_AUTO_EXEC_NEXT);
-    CyDmaTdSetConfiguration(DMA_timing_read_TD[2],  2, DMA_timing_read_TD[3], CY_DMA_TD_AUTO_EXEC_NEXT);
-    CyDmaTdSetConfiguration(DMA_timing_read_TD[3],  2, DMA_timing_read_TD[4], CY_DMA_TD_AUTO_EXEC_NEXT);
-    CyDmaTdSetConfiguration(DMA_timing_read_TD[4],  2, DMA_timing_read_TD[5], CY_DMA_TD_AUTO_EXEC_NEXT);
-    CyDmaTdSetConfiguration(DMA_timing_read_TD[5],  2, DMA_timing_read_TD[6], CY_DMA_TD_AUTO_EXEC_NEXT);
-    CyDmaTdSetConfiguration(DMA_timing_read_TD[6],  2, DMA_timing_read_TD[7], CY_DMA_TD_AUTO_EXEC_NEXT);
-    CyDmaTdSetConfiguration(DMA_timing_read_TD[7],  2, DMA_timing_read_TD[8], CY_DMA_TD_AUTO_EXEC_NEXT);
-    CyDmaTdSetConfiguration(DMA_timing_read_TD[8],  2, DMA_timing_read_TD[9], CY_DMA_TD_AUTO_EXEC_NEXT);
-    CyDmaTdSetConfiguration(DMA_timing_read_TD[9],  2, DMA_timing_read_TD[10], CY_DMA_TD_AUTO_EXEC_NEXT);
-    CyDmaTdSetConfiguration(DMA_timing_read_TD[10], 2, DMA_timing_read_TD[11], CY_DMA_TD_AUTO_EXEC_NEXT);
-    CyDmaTdSetConfiguration(DMA_timing_read_TD[11], 2, DMA_timing_read_TD[12], CY_DMA_TD_AUTO_EXEC_NEXT);
-    CyDmaTdSetConfiguration(DMA_timing_read_TD[12], 2, DMA_timing_read_TD[13], CY_DMA_TD_AUTO_EXEC_NEXT);
-    CyDmaTdSetConfiguration(DMA_timing_read_TD[13], 2, DMA_timing_read_TD[14], CY_DMA_TD_AUTO_EXEC_NEXT);
-    CyDmaTdSetConfiguration(DMA_timing_read_TD[14], 2, DMA_timing_read_TD[15], CY_DMA_TD_AUTO_EXEC_NEXT);
-    CyDmaTdSetConfiguration(DMA_timing_read_TD[15], 2, DMA_timing_read_TD[0], DMA_timing_read__TD_TERMOUT_EN);
-    CyDmaTdSetAddress(DMA_timing_read_TD[0],  LO16((uint32)VIVEDecoder_1_VIVEDecoder_F0_PTR), LO16((uint32)(vive_sensors->sync_pulses + 0)));
-    CyDmaTdSetAddress(DMA_timing_read_TD[1],  LO16((uint32)VIVEDecoder_2_VIVEDecoder_F0_PTR), LO16((uint32)(vive_sensors->sync_pulses + 1)));
-    CyDmaTdSetAddress(DMA_timing_read_TD[2],  LO16((uint32)VIVEDecoder_3_VIVEDecoder_F0_PTR), LO16((uint32)(vive_sensors->sync_pulses + 2)));
-    CyDmaTdSetAddress(DMA_timing_read_TD[3],  LO16((uint32)VIVEDecoder_4_VIVEDecoder_F0_PTR), LO16((uint32)(vive_sensors->sync_pulses + 3)));
-    CyDmaTdSetAddress(DMA_timing_read_TD[4],  LO16((uint32)VIVEDecoder_5_VIVEDecoder_F0_PTR), LO16((uint32)(vive_sensors->sync_pulses + 4)));
-    CyDmaTdSetAddress(DMA_timing_read_TD[5],  LO16((uint32)VIVEDecoder_6_VIVEDecoder_F0_PTR), LO16((uint32)(vive_sensors->sync_pulses + 5)));
-    CyDmaTdSetAddress(DMA_timing_read_TD[6],  LO16((uint32)VIVEDecoder_7_VIVEDecoder_F0_PTR), LO16((uint32)(vive_sensors->sync_pulses + 6)));
-    CyDmaTdSetAddress(DMA_timing_read_TD[7],  LO16((uint32)VIVEDecoder_8_VIVEDecoder_F0_PTR), LO16((uint32)(vive_sensors->sync_pulses + 7)));
-    CyDmaTdSetAddress(DMA_timing_read_TD[8],  LO16((uint32)VIVEDecoder_1_VIVEDecoder_F1_PTR), LO16((uint32)(vive_sensors->timing + 0)));
-    CyDmaTdSetAddress(DMA_timing_read_TD[9],  LO16((uint32)VIVEDecoder_2_VIVEDecoder_F1_PTR), LO16((uint32)(vive_sensors->timing + 1)));
-    CyDmaTdSetAddress(DMA_timing_read_TD[10], LO16((uint32)VIVEDecoder_3_VIVEDecoder_F1_PTR), LO16((uint32)(vive_sensors->timing + 2)));
-    CyDmaTdSetAddress(DMA_timing_read_TD[11], LO16((uint32)VIVEDecoder_4_VIVEDecoder_F1_PTR), LO16((uint32)(vive_sensors->timing + 3)));
-    CyDmaTdSetAddress(DMA_timing_read_TD[12], LO16((uint32)VIVEDecoder_5_VIVEDecoder_F1_PTR), LO16((uint32)(vive_sensors->timing + 4)));
-    CyDmaTdSetAddress(DMA_timing_read_TD[13], LO16((uint32)VIVEDecoder_6_VIVEDecoder_F1_PTR), LO16((uint32)(vive_sensors->timing + 5)));
-    CyDmaTdSetAddress(DMA_timing_read_TD[14], LO16((uint32)VIVEDecoder_7_VIVEDecoder_F1_PTR), LO16((uint32)(vive_sensors->timing + 6)));
-    CyDmaTdSetAddress(DMA_timing_read_TD[15], LO16((uint32)VIVEDecoder_8_VIVEDecoder_F1_PTR), LO16((uint32)(vive_sensors->timing + 7)));
-    CyDmaChSetInitialTd(DMA_timing_read_Chan, DMA_timing_read_TD[0]);
-    CyDmaChEnable(DMA_timing_read_Chan, 1);
+    
+    // DMA Transfer Descriptors (TD) allocation
+    for(int i = 0; i < 16; i++)
+        vive_sensors->DMA_timing_read_TD[i] = CyDmaTdAllocate();
+    
+    // DMA TDs configuration
+    for(int i = 0; i < 15; i++) // 16-1 = 15 because ...
+        CyDmaTdSetConfiguration(vive_sensors->DMA_timing_read_TD[i],  2, vive_sensors->DMA_timing_read_TD[i+1], CY_DMA_TD_AUTO_EXEC_NEXT);
+    CyDmaTdSetConfiguration(vive_sensors->DMA_timing_read_TD[15], 2, vive_sensors->DMA_timing_read_TD[0], DMA_timing_read__TD_TERMOUT_EN); // ... the last one is different
+    
+    // DMA TDs address configuration
+    CyDmaTdSetAddress(vive_sensors->DMA_timing_read_TD[1],  LO16((uint32)VIVEDecoder_2_VIVEDecoder_F0_PTR), LO16((uint32)(vive_sensors->sync_pulses + 1)));
+    CyDmaTdSetAddress(vive_sensors->DMA_timing_read_TD[2],  LO16((uint32)VIVEDecoder_3_VIVEDecoder_F0_PTR), LO16((uint32)(vive_sensors->sync_pulses + 2)));
+    CyDmaTdSetAddress(vive_sensors->DMA_timing_read_TD[3],  LO16((uint32)VIVEDecoder_4_VIVEDecoder_F0_PTR), LO16((uint32)(vive_sensors->sync_pulses + 3)));
+    CyDmaTdSetAddress(vive_sensors->DMA_timing_read_TD[4],  LO16((uint32)VIVEDecoder_5_VIVEDecoder_F0_PTR), LO16((uint32)(vive_sensors->sync_pulses + 4)));
+    CyDmaTdSetAddress(vive_sensors->DMA_timing_read_TD[5],  LO16((uint32)VIVEDecoder_6_VIVEDecoder_F0_PTR), LO16((uint32)(vive_sensors->sync_pulses + 5)));
+    CyDmaTdSetAddress(vive_sensors->DMA_timing_read_TD[6],  LO16((uint32)VIVEDecoder_7_VIVEDecoder_F0_PTR), LO16((uint32)(vive_sensors->sync_pulses + 6)));
+    CyDmaTdSetAddress(vive_sensors->DMA_timing_read_TD[7],  LO16((uint32)VIVEDecoder_8_VIVEDecoder_F0_PTR), LO16((uint32)(vive_sensors->sync_pulses + 7)));
+    CyDmaTdSetAddress(vive_sensors->DMA_timing_read_TD[8],  LO16((uint32)VIVEDecoder_1_VIVEDecoder_F1_PTR), LO16((uint32)(vive_sensors->timing + 0)));
+    CyDmaTdSetAddress(vive_sensors->DMA_timing_read_TD[9],  LO16((uint32)VIVEDecoder_2_VIVEDecoder_F1_PTR), LO16((uint32)(vive_sensors->timing + 1)));
+    CyDmaTdSetAddress(vive_sensors->DMA_timing_read_TD[10], LO16((uint32)VIVEDecoder_3_VIVEDecoder_F1_PTR), LO16((uint32)(vive_sensors->timing + 2)));
+    CyDmaTdSetAddress(vive_sensors->DMA_timing_read_TD[11], LO16((uint32)VIVEDecoder_4_VIVEDecoder_F1_PTR), LO16((uint32)(vive_sensors->timing + 3)));
+    CyDmaTdSetAddress(vive_sensors->DMA_timing_read_TD[12], LO16((uint32)VIVEDecoder_5_VIVEDecoder_F1_PTR), LO16((uint32)(vive_sensors->timing + 4)));
+    CyDmaTdSetAddress(vive_sensors->DMA_timing_read_TD[13], LO16((uint32)VIVEDecoder_6_VIVEDecoder_F1_PTR), LO16((uint32)(vive_sensors->timing + 5)));
+    CyDmaTdSetAddress(vive_sensors->DMA_timing_read_TD[14], LO16((uint32)VIVEDecoder_7_VIVEDecoder_F1_PTR), LO16((uint32)(vive_sensors->timing + 6)));
+    CyDmaTdSetAddress(vive_sensors->DMA_timing_read_TD[15], LO16((uint32)VIVEDecoder_8_VIVEDecoder_F1_PTR), LO16((uint32)(vive_sensors->timing + 7)));
+    
+    CyDmaChSetInitialTd(vive_sensors->DMA_timing_read_Chan, vive_sensors->DMA_timing_read_TD[0]);
+    
+    CyDmaChEnable(vive_sensors->DMA_timing_read_Chan, 1);
 
-    // Triad chips init
-    //vive_sensors_TS4231_init();
+    // TS4231 drivers init
+    for(int i = 0; i < 8; i++)
+        TS4231_driver_init(vive_sensors->ts4231_drivers[i]);
 
     // Start interrupt
     isr_timing_redirect_StartEx(isr_timing_read);
 }
 
-void vive_sensors_TS4231_init() {
-    CyPins_SetPin(data_pins_0);
-    CyPins_SetPin(data_pins_1);
-    CyPins_SetPin(data_pins_2);
-    CyPins_SetPin(data_pins_3);
-    CyPins_SetPin(data_pins_4);
-    CyPins_SetPin(data_pins_5);
-    CyPins_SetPin(data_pins_6);
-    CyPins_SetPin(data_pins_7);
-
-    CyPins_SetPin(enveloppe_pins_0);
-    CyPins_SetPin(enveloppe_pins_1);
-    CyPins_SetPin(enveloppe_pins_2);
-    CyPins_SetPin(enveloppe_pins_3);
-    CyPins_SetPin(enveloppe_pins_4);
-    CyPins_SetPin(enveloppe_pins_5);
-    CyPins_SetPin(enveloppe_pins_6);
-    CyPins_SetPin(enveloppe_pins_7);
-
-    CyPins_SetPinDriveMode(data_pins_0, CY_PINS_DM_STRONG);
-    CyPins_SetPinDriveMode(data_pins_1, CY_PINS_DM_STRONG);
-    CyPins_SetPinDriveMode(data_pins_2, CY_PINS_DM_STRONG);
-    CyPins_SetPinDriveMode(data_pins_3, CY_PINS_DM_STRONG);
-    CyPins_SetPinDriveMode(data_pins_4, CY_PINS_DM_STRONG);
-    CyPins_SetPinDriveMode(data_pins_5, CY_PINS_DM_STRONG);
-    CyPins_SetPinDriveMode(data_pins_6, CY_PINS_DM_STRONG);
-    CyPins_SetPinDriveMode(data_pins_7, CY_PINS_DM_STRONG);
-
-    CyPins_SetPinDriveMode(enveloppe_pins_0, CY_PINS_DM_STRONG);
-    CyPins_SetPinDriveMode(enveloppe_pins_1, CY_PINS_DM_STRONG);
-    CyPins_SetPinDriveMode(enveloppe_pins_2, CY_PINS_DM_STRONG);
-    CyPins_SetPinDriveMode(enveloppe_pins_3, CY_PINS_DM_STRONG);
-    CyPins_SetPinDriveMode(enveloppe_pins_4, CY_PINS_DM_STRONG);
-    CyPins_SetPinDriveMode(enveloppe_pins_5, CY_PINS_DM_STRONG);
-    CyPins_SetPinDriveMode(enveloppe_pins_6, CY_PINS_DM_STRONG);
-    CyPins_SetPinDriveMode(enveloppe_pins_7, CY_PINS_DM_STRONG);
-
-    CyPins_ClearPin(enveloppe_pins_0);
-    CyPins_ClearPin(enveloppe_pins_1);
-    CyPins_ClearPin(enveloppe_pins_2);
-    CyPins_ClearPin(enveloppe_pins_3);
-    CyPins_ClearPin(enveloppe_pins_4);
-    CyPins_ClearPin(enveloppe_pins_5);
-    CyPins_ClearPin(enveloppe_pins_6);
-    CyPins_ClearPin(enveloppe_pins_7);
-
-    CyDelay(2);
-
-    CyPins_ClearPin(data_pins_0);
-    CyPins_ClearPin(data_pins_1);
-    CyPins_ClearPin(data_pins_2);
-    CyPins_ClearPin(data_pins_3);
-    CyPins_ClearPin(data_pins_4);
-    CyPins_ClearPin(data_pins_5);
-    CyPins_ClearPin(data_pins_6);
-    CyPins_ClearPin(data_pins_7);
-
-    CyDelayUs(10);
-
-    CyPins_SetPin(enveloppe_pins_0);
-    CyPins_SetPin(enveloppe_pins_1);
-    CyPins_SetPin(enveloppe_pins_2);
-    CyPins_SetPin(enveloppe_pins_3);
-    CyPins_SetPin(enveloppe_pins_4);
-    CyPins_SetPin(enveloppe_pins_5);
-    CyPins_SetPin(enveloppe_pins_6);
-    CyPins_SetPin(enveloppe_pins_7);
-
-    CyDelayUs(100);
-
-    CyPins_SetPinDriveMode(data_pins_0, CY_PINS_DM_DIG_HIZ);
-    CyPins_SetPinDriveMode(data_pins_1, CY_PINS_DM_DIG_HIZ);
-    CyPins_SetPinDriveMode(data_pins_2, CY_PINS_DM_DIG_HIZ);
-    CyPins_SetPinDriveMode(data_pins_3, CY_PINS_DM_DIG_HIZ);
-    CyPins_SetPinDriveMode(data_pins_4, CY_PINS_DM_DIG_HIZ);
-    CyPins_SetPinDriveMode(data_pins_5, CY_PINS_DM_DIG_HIZ);
-    CyPins_SetPinDriveMode(data_pins_6, CY_PINS_DM_DIG_HIZ);
-    CyPins_SetPinDriveMode(data_pins_7, CY_PINS_DM_DIG_HIZ);
-
-    CyPins_SetPinDriveMode(enveloppe_pins_0, CY_PINS_DM_DIG_HIZ);
-    CyPins_SetPinDriveMode(enveloppe_pins_1, CY_PINS_DM_DIG_HIZ);
-    CyPins_SetPinDriveMode(enveloppe_pins_2, CY_PINS_DM_DIG_HIZ);
-    CyPins_SetPinDriveMode(enveloppe_pins_3, CY_PINS_DM_DIG_HIZ);
-    CyPins_SetPinDriveMode(enveloppe_pins_4, CY_PINS_DM_DIG_HIZ);
-    CyPins_SetPinDriveMode(enveloppe_pins_5, CY_PINS_DM_DIG_HIZ);
-    CyPins_SetPinDriveMode(enveloppe_pins_6, CY_PINS_DM_DIG_HIZ);
-    CyPins_SetPinDriveMode(enveloppe_pins_7, CY_PINS_DM_DIG_HIZ);
+void VIVE_sensors_process_pulses(VIVE_sensors *vive_sensors, VIVE_sensors_data* vive_sensors_data) {
+    uint8_t timing_readable_reg_value = timing_readable_Read();
+    
+    // For each sensor
+    for(int i = 0; i < 8; i++) {
+        vive_sensors_data->angles[i] = ANGLE_invalid_value;
+        
+        bool timing_readable_bit = (timing_readable_reg_value & (1 << i)) >> i;        
+        if(timing_readable_bit) { // If it's readable
+            // convert timing to angles
+            uint16_t timing = vive_sensors->timing[i];
+            
+            // constrain
+            if(timing>TIMING_angle_max_tick) {
+                timing = TIMING_angle_max_tick;
+            }
+            else if(timing<TIMING_angle_min_tick) {
+                timing = TIMING_angle_min_tick;
+            }
+            
+            vive_sensors_data->angles[i] = CY_M_PI*((timing-TIMING_angle_center_tick)/TIMING_cycle_max_tick);
+        }
+    }
 }
+
+/*
+    --------------------------
+    Interrupt Service Routines
+    --------------------------
+*/
 
 CY_ISR(isr_timing_read) {
     // Clear pending bit (from interrupt API)
     isr_timing_redirect_ClearPending();
 
-    VIVE_pulses_decoded = 1;
+    // Set the flag
+    VIVE_pulses_decoded = true;
 }
 
 /* [] END OF FILE */
--- code/ViveBeacons/ViveBeacons.cydsn/vive_sensors.h
+++ code/ViveBeacons/ViveBeacons.cydsn/vive_sensors.h
@@ -10,19 +10,55 @@
  * ========================================
 */
 
-#include "project.h"
+#if !defined(VIVE_SENSORS_H)
+#define VIVE_SENSORS_H
 
-/* Defines for DMA_timing_read */
+#include <cytypes.h>
+#include <stdbool.h>
+#include "project.h"
+#include "TS4231_driver.h"
+
+// Defines
 #define DMA_timing_read_BYTES_PER_BURST 127
 #define DMA_timing_read_REQUEST_PER_BURST 0
 #define DMA_timing_read_SRC_BASE (CYDEV_PERIPH_BASE)
 #define DMA_timing_read_DST_BASE (CYDEV_SRAM_BASE)
 
-typedef struct vive_sensors vive_sensors;
-struct vive_sensors {
+#define TIMING_cycle_max_tick 60000
+#define TIMING_cycle_divided_by_3_tick TIMING_cycle_max_tick/3
+
+#define TIMING_angle_center_tick 28800
+#define TIMING_angle_min_tick TIMING_angle_center_tick - TIMING_cycle_divided_by_3_tick
+#define TIMING_angle_max_tick TIMING_angle_center_tick + TIMING_cycle_divided_by_3_tick
+
+#define ANGLE_invalid_value -42
+    
+#define CLK_frequency 7200000 // in Hertz
+
+// Enums
+typedef enum Names Names;
+enum Names {STIFF, NIVIDI, PIRNOIR, ARMEN, VIELLE, MILLIER, PETITMINOU, KEREON};
+
+// Attributes
+typedef struct VIVE_sensors VIVE_sensors;
+struct VIVE_sensors {
+    // TS4231_drivers
+    TS4231_driver* ts4231_drivers[8];
+    
+    // Variable declarations for DMA_timing_read
+    uint8 DMA_timing_read_Chan;
+    uint8 DMA_timing_read_TD[16];
+    
     // Raw values
-    uint16_t sync_pulses[8];
-    uint16_t timing[8];
+    volatile uint16_t sync_pulses[8];
+    volatile uint16_t timing[8];
+};
+
+// Data structure for transferring VIVE sensors data to Position_finder
+typedef struct VIVE_sensors_data VIVE_sensors_data;
+struct VIVE_sensors_data {
+    // Processed angles
+    double angles[8];
     
     // sync pulses decoded
     uint8_t skip;
@@ -30,10 +66,17 @@ struct vive_sensors {
     uint8_t axis;
 };
 
-void vive_sensors_init(vive_sensors *vive_sensors);
-void vive_sensors_TS4231_init();
+// Global variable
+volatile bool VIVE_pulses_decoded;
 
+// Public methods
+VIVE_sensors* VIVE_sensors_create();
+void VIVE_sensors_init(VIVE_sensors *vive_sensors);
+void VIVE_sensors_process_pulses(VIVE_sensors *vive_sensors, VIVE_sensors_data* vive_sensors_data);
+
+// Interrupt Service Routines
 CY_ISR_PROTO(isr_timing_read);
-volatile char VIVE_pulses_decoded;
+
+#endif
 
 /* [] END OF FILE */
